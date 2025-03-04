8d71e8253a7f647b0c63ef6eee87d1a9160d765c
swaminaing <smnaing9.smn@gmail.com>
2025-03-04 23:01:49 +0630

    tailwind css code for basic ui components

---
+++ src/utils/http.js
@@ -0,0 +1,15 @@
+const url = "https://randomuser.me/api/?results=10";
+
+export async function getUsers(){
+    try{
+        const response = await fetch(url);
+
+        if(!response.ok){
+            throw new Error("Something's wrong");
+        }
+
+        return response.json();
+    }catch(err){
+        throw new err;
+    }
+}
